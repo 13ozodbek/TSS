265ddb9fb9aceb099809610f2c58f91aed789b19
ozodbek <ozodbekkkk13@icloud.com>
2024-07-09 01:21:07 +0500

    deleted JWT tokenobtain from urls.py

--- management/urls.py
+++ management/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
-
 from .views import Register, Login, ResetPassword, UpdateProfile, UserInfoView
 
 urlpatterns = [
-    path('api/refresh/', TokenRefreshView.as_view(), name='refresh'),
-    path('api/get/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
 
     path('auth/register/', Register.as_view({'post': 'register'})),
     path('auth/verify/', Register.as_view({'post': 'verify'})),
