d130b7aa084c227f3f4a82271a41d01d693deeaa
ozodbek <ozodbekkkk13@icloud.com>
2024-07-10 19:34:34 +0500

    -m

--- management/urls.py
+++ management/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.conf import settings
+from django.urls import path, re_path
 from .views import Register, Login, ResetPassword, UpdateProfile, UserInfoView
-
+from django.views.static import serve
 urlpatterns = [
 
 
@@ -17,6 +18,9 @@ urlpatterns = [
     path('auth/login/', Login.as_view({'post': 'login'})),
 
     path('auth/me/',UserInfoView.as_view({'post':'auth_me'})),
+
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 
 
