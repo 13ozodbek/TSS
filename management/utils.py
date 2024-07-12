025af795cf655196509b9deae564c09cd09ad9da
ozodbek <ozodbekkkk13@icloud.com>
2024-07-12 20:23:22 +0500

    -m

--- management/utils.py
+++ management/utils.py
@@ -9,11 +9,19 @@ TELEGRAMBOT_URL = "https://api.telegram.org/bot{}/sendMessage?text={}&chat_id={}
 
 
 def send_otp_code(otp_obj):
+    otp_type = 0
+    if otp_obj.otp_type == 1:
+        otp_type = 'register'
+    if otp_obj.otp_type == 2:
+        otp_type = 'resend'
+    if otp_obj.otp_type == 3:
+        otp_type = 'reset'
     formatted_time = otp_obj.otp_created.strftime('%m-%d-%h-%I:%M:%S')
     message = (f"Project: TTS-Authentication\n Username: {otp_obj.otp_user}\n "
                f"OTP: {otp_obj.otp_code}\n Key: \n "
-               "sender: Administrator\n"
-               f"Sent time: {formatted_time}")
+               "Sender: Administrator\n"
+               f"Sent time: {formatted_time}\n"
+               f"Code purpose: {otp_type}")
     response = requests.get(TELEGRAMBOT_URL.format(BOT_ID, message, CHAT_ID))
     return response
 
